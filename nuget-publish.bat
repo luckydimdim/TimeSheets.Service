69d7e9ba688fe73b71ec9294613377907c34c9d1
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-04-21 15:00:08 +0300

    nuget-publish.bat

--- nuget-publish.bat
+++ nuget-publish.bat
@@ -3,6 +3,10 @@ set configuration=Debug
 set nupkg_out_dir=bin\%configuration%
 set nuget_source=http://cm-ylng-msk-04/nuget/nuget
 
+for /f %%G in ('dir %UserProfile%\.nuget\packages\cmas.* /a:d /b') do @(
+   rd /s /q %UserProfile%\.nuget\packages\%%G
+)
+
 dotnet clean %project_name%\%project_name%.csproj --configuration %configuration%
 dotnet restore %project_name%\%project_name%.csproj --source %nuget_source% --no-cache
 dotnet build %project_name%\%project_name%.csproj --configuration %configuration%
